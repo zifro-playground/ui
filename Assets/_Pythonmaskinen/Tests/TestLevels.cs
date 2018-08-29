3af94c8f1bc3be0f61c063679f9198aff1bc63aa
Unknown <jensbern@kth.se>
2018-08-29 15:06:36 +0200

    added Test button in unity  editor

--- Assets/_Pythonmaskinen/Tests/TestLevels.cs
+++ Assets/_Pythonmaskinen/Tests/TestLevels.cs
@@ -1,42 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace PM
 {
     public class TestLevels : MonoBehaviour, IPMLevelCompleted, IPMCompilerStopped
     {
-        private List<TestError> testErrors = new List<TestError>(); 
+        public bool IsTesting;
+        public static TestLevels Instance;
+        private List<TestError> testErrors = new List<TestError>();
 
-        // Use this for initialization
-        void Start()
+        private void Awake()
         {
-            
+            if (Instance == null)
+                Instance = this;
         }
-
-        // Update is called once per frame
-        void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.U))
-                print(UISingleton.instance.textField.text.text);
-
-            if (Input.GetKeyDown(KeyCode.C))
-            {
-                TestLevel();
-            }
-
-        }
-
 
         public void OnPMLevelCompleted()
         {
-            TestNextLevel();
+            if(IsTesting)
+                TestNextLevel();
         }
-
-
+        
         public void TestLevel()
         {
-            PMWrapper.mainCode = Main.Instance.LevelData.levelSettings.exampleSolutionCode;
+            if(Main.Instance.LevelData.levelSettings != null && Main.Instance.LevelData.levelSettings.exampleSolutionCode != null)
+                PMWrapper.mainCode = Main.Instance.LevelData.levelSettings.exampleSolutionCode;
             PMWrapper.speedMultiplier = 1;
             PMWrapper.RunCode();
         }
@@ -50,20 +38,22 @@ namespace PM
             }
             else
             {
-                print(testErrors.Count);
-                foreach(TestError te in testErrors)
+                foreach (TestError te in testErrors)
                 {
-                    print(te.levelId + " " + te.errorType);
+                    print("LevelID: " + te.levelId + " ErrorType: " + te.errorType + "\n");
                 }
+                print("Testing completed\n");
+                IsTesting = false;
             }
         }
 
         public void OnPMCompilerStopped(HelloCompiler.StopStatus status)
         {
+            if(!IsTesting)
+                return; 
             if (status == HelloCompiler.StopStatus.RuntimeError || status == HelloCompiler.StopStatus.TaskError)
             {
                 testErrors.Add(new TestError(Main.Instance.LevelData.id, status.ToString()));
-                //print("Runtime Error on level: " + Main.Instance.LevelData.id);
                 TestNextLevel();
             }
         }
