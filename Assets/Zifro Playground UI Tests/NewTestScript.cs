464e71623c4293db2766516f98ee4f326b39d12f
kalle (jag) <kalle.jillheden@gmail.com>
2019-04-02 21:04:03 +0200

    Added tests

--- Assets/Zifro Playground UI Tests/NewTestScript.cs
+++ Assets/Zifro Playground UI Tests/NewTestScript.cs
@@ -4,25 +4,36 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-namespace Tests
+public class NewTestScript
 {
-    public class NewTestScript
-    {
-        // A Test behaves as an ordinary method
-        [Test]
-        public void NewTestScriptSimplePasses()
-        {
-            // Use the Assert class to test conditions
-        }
+	// A Test behaves as an ordinary method
+	[Test]
+	public void NewTestScriptSimplePasses()
+	{
+		// Use the Assert class to test conditions
+	}
 
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
-        [UnityTest]
-        public IEnumerator NewTestScriptWithEnumeratorPasses()
-        {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            yield return null;
-        }
-    }
+	// A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+	// `yield return null;` to skip a frame.
+	[UnityTest]
+	public IEnumerator NewTestScriptWithEnumeratorPasses()
+	{
+		// Use the Assert class to test conditions.
+		// Use yield to skip a frame.
+		yield return null;
+	}
+
+	[Test]
+	public void NewTestScriptSimpleFails()
+	{
+		Assert.Fail("Scheduled to fail!");
+	}
+
+	[TestCase("foo")]
+	[TestCase("bar")]
+	[TestCase("")]
+	public void NewTestScriptParameterizedTest(string arg1)
+	{
+		Assert.Pass($"passed: \"{arg1}\"");
+	}
 }
