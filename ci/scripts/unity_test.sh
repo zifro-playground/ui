a9a4b9e5b1afad453dca3cf08614c696d18c40eb
kalle (jag) <kalle.jillheden@gmail.com>
2019-04-08 11:58:01 +0200

    [ci] Added testing of both playmode and editmode

--- ci/scripts/unity_test.sh
+++ ci/scripts/unity_test.sh
@@ -6,63 +6,77 @@ set +o errexit
 set -o pipefail
 
 PROJECT=${1?Project path}
-TEST_RESULTS=${2:-$PROJECT/test-results.xml}
-
-echo ">>>>>> Executing Unity"
-echo
-
-${UNITY_EXECUTABLE:-xvfb-run -as '-screen 0 640x480x24' /opt/Unity/Editor/Unity} \
-    -projectPath $PROJECT \
-    -runEditorTests \
-    -editorTestsResultFile $TEST_RESULTS \
-    -batchmode \
-    -buildTarget Linux \
-    -logfile
-
-UNITY_EXIT_CODE=$?
-
-echo
-echo "<<<<<< Unity execution complete"
-echo
-
-if [ $UNITY_EXIT_CODE -eq 0 ]; then
-  echo "Run succeeded, no failures occurred";
-elif [ $UNITY_EXIT_CODE -eq 2 ]; then
-  echo "Run succeeded, some tests failed";
-elif [ $UNITY_EXIT_CODE -eq 3 ]; then
-  echo "Run failure (other failure)";
-else
-  echo "Unexpected exit code $UNITY_EXIT_CODE";
-  exit $UNITY_EXIT_CODE
-fi
-
-echo
-echo ">>>>>> Gathering test results"
-echo
+TEST_RESULTS_FOLDER=${2:-$PROJECT}
+PLATFORMS="editmode\nplaymode"
 
 passed=$((0))
 failed=$((0))
 skipped=$((0))
 total=$((0))
 
-regexTestsPassed='passed="([0-9]+)"'
-regexTestsFailed='failed="([0-9]+)"'
-regexTestsInconc='inconclusive="([0-9]+)"'
-regexTestsSkippe='skipped="([0-9]+)"'
+while read platform
+do
+  echo
+  echo ">>>>>> Executing Unity '$platform' tests"
 
-results="$(cat $TEST_RESULTS | grep "<test-run")"
+  test_results_file=$TEST_RESULTS_FOLDER/test-results-$platform.xml
+  echo "Results file: $test_results_file"
+  echo
 
-[[ $results =~ $regexTestsPassed ]] && [[ "${BASH_REMATCH[1]}" ]] && ((passed=${BASH_REMATCH[1]}))
-echo "Passed: $passed"
-[[ $results =~ $regexTestsFailed ]] && [[ "${BASH_REMATCH[1]}" ]] && ((failed=${BASH_REMATCH[1]}))
-[[ $results =~ $regexTestsInconc ]] && [[ "${BASH_REMATCH[1]}" ]] && ((failed+=${BASH_REMATCH[1]}))
-echo "Failed: $failed"
-[[ $results =~ $regexTestsSkippe ]] && [[ "${BASH_REMATCH[1]}" ]] && ((skipped=${BASH_REMATCH[1]}))
-echo "Skipped: $skipped"
+  ${UNITY_EXECUTABLE:-xvfb-run -as '-screen 0 640x480x24' /opt/Unity/Editor/Unity} \
+      -projectPath $PROJECT \
+      -runTests \
+      -testPlatform $platform \
+      -testResults $test_results_file \
+      -buildTarget Linux \
+      -batchmode \
+      -logfile
+
+  UNITY_EXIT_CODE=$?
+
+  echo
+  echo "<<<<<< Unity '$platform' execution complete"
+  echo
+
+  if [ $UNITY_EXIT_CODE -eq 0 ]; then
+    echo "Run succeeded, no failures occurred";
+  elif [ $UNITY_EXIT_CODE -eq 2 ]; then
+    echo "Run succeeded, some tests failed";
+  elif [ $UNITY_EXIT_CODE -eq 3 ]; then
+    echo "Run failure (other failure)";
+  else
+    echo "Unexpected exit code $UNITY_EXIT_CODE";
+    exit $UNITY_EXIT_CODE
+  fi
+
+  echo
+  echo ">>>>>> Gathering '$platform' test results"
+  echo
+
+  regexTestsPassed='passed="([0-9]+)"'
+  regexTestsFailed='failed="([0-9]+)"'
+  regexTestsInconc='inconclusive="([0-9]+)"'
+  regexTestsSkippe='skipped="([0-9]+)"'
+
+  results="$(cat $test_results_file | grep "<test-run")"
+
+  old_passed=$((passed))
+  [[ $results =~ $regexTestsPassed ]] && [[ "${BASH_REMATCH[1]}" ]] && ((passed+=${BASH_REMATCH[1]}))
+  echo "Passed: $passed (+$((passed-old_passed)))"
+  old_failed=$((failed))
+  [[ $results =~ $regexTestsInconc ]] && [[ "${BASH_REMATCH[1]}" ]] && ((failed+=${BASH_REMATCH[1]}))
+  [[ $results =~ $regexTestsFailed ]] && [[ "${BASH_REMATCH[1]}" ]] && ((failed+=${BASH_REMATCH[1]}))
+  echo "Failed: $failed (+$((failed-old_failed)))"
+  old_skipped=$((skipped))
+  [[ $results =~ $regexTestsSkippe ]] && [[ "${BASH_REMATCH[1]}" ]] && ((skipped+=${BASH_REMATCH[1]}))
+  echo "Skipped: $skipped (+$((skipped-old_skipped)))"
+  echo
+
+done < <(echo -e "$PLATFORMS")
 
 ((total=passed+failed+skipped))
 
-echo "Total: $total"
+echo "Total: $total tests"
 
 echo "export TEST_PASSED=$(($passed))" >> $BASH_ENV
 echo "export TEST_FAILED=$(($failed))" >> $BASH_ENV
