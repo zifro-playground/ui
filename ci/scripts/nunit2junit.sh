52f62db8a90a0f8690d05cfd1b41fc8a5a5a8d01
kalle (jag) <kalle.jillheden@gmail.com>
2019-04-08 12:18:46 +0200

    [ci] Fixed nUnit2jUnit script

--- ci/scripts/nunit2junit.sh
+++ ci/scripts/nunit2junit.sh
@@ -4,8 +4,10 @@ INPUT_FOLDER=${1?Folder of input NUnit XML files}
 OUTPUT_FOLDER=${2?Folder of output JUnit XML files}
 : ${PWSH_SCRIPT:=/usr/local/bin/nunit2junit.ps1}
 
+echo "Using powershell script '$PWSH_SCRIPT'"
 echo "Converting NUnit XML files from folder '$INPUT_FOLDER'"
 echo "Outputting JUnit XML files into folder '$OUTPUT_FOLDER'"
+echo
 
 (cd $INPUT_FOLDER && find . -name '*.xml') | cut -c 3- |
 while read path
@@ -14,11 +16,13 @@ do
     file="$(basename "$path")"
     echo "Working on '$folder' :: '$file'"
 
-    abs_folder=$OUTPUT_FOLDER/$folder
-    abs_file=$abs_folder/$file
+    out_folder=$OUTPUT_FOLDER/$folder
+    out_file=$out_folder/$file
 
-    mkdir -p $abs_folder
-    pwsh $PWSH_SCRIPT $path $abs_folder/$file
+    in_file=$INPUT_FOLDER/$path
+
+    mkdir -p $out_folder
+    pwsh $PWSH_SCRIPT $in_file $out_file
 done
 
 echo "Convertion complete"
