3263fe9de7fb4c5fe5de652865a834eaf070256f
kalle (jag) <kalle.jillheden@gmail.com>
2019-04-12 10:25:41 +0200

    Upped version to 2.1.2

--- Assets/Zifro Playground UI/ZifroPlaygroundUIAssemblyInfo.cs
+++ Assets/Zifro Playground UI/ZifroPlaygroundUIAssemblyInfo.cs
@@ -4,9 +4,9 @@ using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEngine;
 
-[assembly: AssemblyVersion("2.1.1")]
-[assembly: AssemblyFileVersion("2.1.1")]
-[assembly: AssemblyInformationalVersion("2.1.1")]
+[assembly: AssemblyVersion("2.1.2")]
+[assembly: AssemblyFileVersion("2.1.2")]
+[assembly: AssemblyInformationalVersion("2.1.2")]
 
 namespace PM
 {
