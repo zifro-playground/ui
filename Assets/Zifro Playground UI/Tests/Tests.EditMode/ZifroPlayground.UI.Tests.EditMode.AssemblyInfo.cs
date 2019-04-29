1806b3e7ec6815d38eb9677261f49833d64e6967
kalle (jag) <kalle.jillheden@gmail.com>
2019-04-29 14:42:05 +0200

    Upped to 2.4.1

--- Assets/Zifro Playground UI/Tests/Tests.EditMode/ZifroPlayground.UI.Tests.EditMode.AssemblyInfo.cs
+++ Assets/Zifro Playground UI/Tests/Tests.EditMode/ZifroPlayground.UI.Tests.EditMode.AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.4.0")]
-[assembly: AssemblyFileVersion("2.4.0")]
-[assembly: AssemblyInformationalVersion("2.4.0")]
+[assembly: AssemblyVersion("2.4.1")]
+[assembly: AssemblyFileVersion("2.4.1")]
+[assembly: AssemblyInformationalVersion("2.4.1")]
